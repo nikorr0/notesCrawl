66885870590f190b479a3bad1ea4cd91ced69e71
zalberix <zalberix@gmail.com>
2023-12-02 23:16:10 +0500

    Добавлена индексация TF-IDF

--- tf_idf_converter.py
+++ tf_idf_converter.py
@@ -19,3 +19,25 @@
 #
 # Подача и возвращение словаря нужно для того, чтобы в итоге (после поиска заметок) вывоводить самые подходящие исходные заметки по их индексам.
 # Подача запроса в виде словаря сделана, чтобы в одну и ту же функцию можно было подать и запрос, и тексты (можно просто if сделать и не подавать словарь при запросе)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# testDataset = [
+#     "кабинет соцгум восстановить пропуск",
+#     "восстановить отчисление прийти кабинет 999",
+#     "приказ отчисление университет 92"
+# ]
+
+def getTFIDFWidthFromNotes(normalayzQuery, normalazeNotes):
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_vectorizer.fit(normalazeNotes)
+    tfidf_query = tfidf_vectorizer.transform([normalayzQuery])
+    tfidf_notes = tfidf_vectorizer.transform(normalazeNotes)
+    
+    return {
+        "query": tfidf_query.toarray(), 
+        "notes": tfidf_notes.toarray()
+    }
+
+# queryAndNotesTFIDF = getTFIDFWidthFromNotes("соцгум университет",testDataset)
+# print(queryAndNotesTFIDF)
