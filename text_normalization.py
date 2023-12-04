0956a1b248500361ef8b5ce31630f8d58066bcd1
DLegio <legionofdandy@mail.ru>
2023-12-04 17:46:33 +0500

    добавляю text_normalizatio

--- text_normalization.py
+++ text_normalization.py
@@ -1,6 +1,4 @@
-# Это делаю я (Никита)
-#
-# На вход подается словарь из текстов и их номеров или список из одного запроса и его номера (0)
+# На вход подается словарь из текстов и их номеров или словарь из одного запроса и его номера (0)
 # На выход возвращается словарь из нормализованных текстов (слова разделены пробелом)
 #
 # Пример 1 (запрос):
@@ -15,3 +13,50 @@
 #
 # Подача и возвращение словаря нужно для того, чтобы в итоге (после поиска заметок) вывоводить самые подходящие исходные заметки по их индексам.
 # Подача запроса в виде словаря сделана, чтобы в одну и ту же функцию можно было подать и запрос, и тексты
+
+import string
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import pymorphy2
+
+def nomalize(texts_dict, mode="dict_texts"):
+    morph_analyzer = pymorphy2.MorphAnalyzer()
+    stop_words = stopwords.words('russian')
+
+    if mode == "query":
+        if type(texts_dict) == type(str()):
+            texts = [texts_dict]
+        if type(texts_dict) == type(list()):
+            texts = texts_dict
+        if type(texts_dict) == type(dict()):
+            mode="dict_texts"
+
+    if mode != "query":
+        texts = [text for text in texts_dict.values()]
+        ids = [id for id in texts_dict.keys()]
+
+    norm_texts = list()
+    for text in texts:
+        for p in string.punctuation:
+            text = text.replace(p, '')
+        text = text.lower()
+        text = re.sub('[0-9]+', '', text)
+        norm_words = list()
+
+        for word in word_tokenize(text):
+            if word in stop_words:
+                continue
+            else:
+                word = morph_analyzer.parse(word)[0].normal_form
+                norm_words.append(word)
+        norm_texts.append(norm_words)
+
+    if mode == "query":
+        return " ".join(norm_texts[0])
+
+    norm_texts_dict = dict()
+    for id, text in zip(ids, norm_texts):
+        norm_texts_dict[id] = " ".join(text)
+    
+    return norm_texts_dict
